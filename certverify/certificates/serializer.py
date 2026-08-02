c276b979ff0744b57f563e7439c96804c1d42a93
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2026-08-02 15:04:03 +0100

    feat: Add admin and public Certificate serializers

---
+++ certverify/certificates/serializer.py
@@ -0,0 +1,26 @@
+from .models import Certificate 
+from rest_framework import serializers
+
+
+
+class CertificateSerializer(serializers.ModelSerializer):
+    is_valid = serializers.ReadOnlyField()
+    
+    class Meta:
+        model = Certificate
+        fields = [
+    
+            'id', 'recipient', 'course', 'issue_date', 'expiry_date',
+            'is_revoked', 'remarks', 'is_valid', 'created_at', 'updated_at',
+        ]
+        read_only_fields = ['id', 'created_at', 'updated_at']
+                
+                
+class CertificatePublicSerializer(serializers.ModelSerializer):
+    is_valid = serializers.ReadOnlyField()
+    
+    
+    
+    class Meta:
+        model = Certificate
+        fields = [' recipient', 'course', 'issue_date', 'expiry_date', 'is_valid']                
