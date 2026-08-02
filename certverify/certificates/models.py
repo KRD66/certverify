20b339f8d5a6f0f89cc65710915faba7bad09e1e
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2026-08-02 11:40:28 +0100

    Add Certificate model with is_valid property

--- certverify/certificates/models.py
+++ certverify/certificates/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import uuid 
+from django.utils import timezone
 
-# Create your models here.
+class Certificate(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    recipient = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    expiry_date = models.DateTimeField(null=True,blank=True)
+    is_revoked = models.BooleanField(default = False)
+    remarks = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    course = models.CharField(max_length=150)
+    
+    
+    @property
+    def is_valid(self):
+        if self.is_revoked:
+            return False
+        if self.expiry_date and self.expiry_date < timezone.now():
+            return False
+        return True
+    
+    
+    def __str__(self):
+        return f"{self.recipient} - {self.course} ({self.id})" 
+            
