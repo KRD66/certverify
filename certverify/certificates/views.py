eb90d0011ccd0271f523883c479079c28689db65
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2026-08-19 09:31:52 +0100

    Fix search.html (was overwritten with home.html content), restrict search/issue to staff, add QR and download button to verify page

--- certverify/certificates/views.py
+++ certverify/certificates/views.py
@@ -10,35 +10,33 @@ from .forms import CertificateIssueForm
 
 
 def home_view(request):
-    return render (request, "certificates/home.html")
+    return render(request, "certificates/home.html")
 
 
+@staff_member_required
 def search_view(request):
-     query = request.GET.get("q", "") 
-     
-     if not query:
-         return render(request, "certificates/search.html", {"results":[]})
-     results  =  Certificate.objects.filter(recipient__icontains=query)
-     return render(request, "certificates/search.html", {"results":results})
+    query = request.GET.get("q", "")
+
+    if not query:
+        return render(request, "certificates/search.html", {"results": []})
+    results = Certificate.objects.filter(recipient__icontains=query)
+    return render(request, "certificates/search.html", {"results": results})
 
 
-
-def verify_view(request,cert_id):
+def verify_view(request, cert_id):
     certificate = get_object_or_404(Certificate, id=cert_id)
-    
     return render(request, "certificates/verify.html", {"certificate": certificate})
 
+
 class CertificateListView(generics.ListCreateAPIView):
-    queryset =  Certificate.objects.all()
+    queryset = Certificate.objects.all()
     serializer_class = CertificateSerializer
-    
- 
+
+
 class CertificateDetailView(generics.RetrieveUpdateAPIView):
     queryset = Certificate.objects.all()
     serializer_class = CertificateSerializer
-    
-    
-        
+
 
 def download_certificate_pdf(request, cert_id):
     certificate = get_object_or_404(Certificate, id=cert_id)
@@ -58,8 +56,5 @@ def issue_certificate_view(request):
             return redirect("verify", cert_id=certificate.id)
     else:
         form = CertificateIssueForm()
-            
-    return render(request, "certificates/issue.html", {"form": form}) 
 
-
-
+    return render(request, "certificates/issue.html", {"form": form})
