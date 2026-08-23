4bc05829b8a6e9bfe6c835d7151f4a49fcd72f9b
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2026-08-23 14:28:45 +0100

    fix:allowed host fixed

--- certverify/certverify/settings.py
+++ certverify/certverify/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-abc123...')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost','certverify.pxxl.click').split(',')
 
 
 # Application definition
